fed5a9db979fa46c975debdd9e5afd23cb99cd53
aparna1129 <99273305+aparna1129@users.noreply.github.com>
2024-09-09 16:33:25 +0530

    Time: 2 ms (98.44%), Space: 45.3 MB (5.6%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> complements = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            Integer complementIndex = complements.get(nums[i]);
+            if(complementIndex != null){
+                return new int[]{complementIndex, i};
+            }
+            complements.put(target - nums[i], i);
+        }
+        return nums;
+    }
+}
+
+
+// rem    , indices
+// 9-2 = 7,   0
