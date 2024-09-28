76c6623887ee9500a570a540afd73a01a30e7ed8
aparna1129 <99273305+aparna1129@users.noreply.github.com>
2024-09-28 21:11:27 +0530

    Time: 5 ms (37.86%), Space: 45.2 MB (6.79%) - LeetHub

---
+++ 0641-design-circular-deque/0641-design-circular-deque.java
@@ -0,0 +1,82 @@
+class MyCircularDeque {
+    private LinkedList<Integer> deq;
+    private int cnt;
+    private int k;
+
+    public MyCircularDeque(int k) {
+        this.k = k;
+        this.deq = new LinkedList<>();
+        this.cnt = 0;
+    }
+    
+    public boolean insertFront(int value) {
+        if (cnt == k) {
+            return false;
+        }
+        deq.addFirst(value);
+        cnt++;
+        return true;
+    }
+    
+    public boolean insertLast(int value) {
+        if (cnt == k) {
+            return false;
+        }
+        deq.addLast(value);
+        cnt++;
+        return true;
+    }
+    
+    public boolean deleteFront() {
+        if (cnt == 0) {
+            return false;
+        }
+        deq.removeFirst();
+        cnt--;
+        return true;
+    }
+    
+    public boolean deleteLast() {
+        if (cnt == 0) {
+            return false;
+        }
+        deq.removeLast();
+        cnt--;
+        return true;
+    }
+    
+    public int getFront() {
+        if (cnt == 0) {
+            return -1;
+        }
+        return deq.getFirst();
+    }
+    
+    public int getRear() {
+        if (cnt == 0) {
+            return -1;
+        }
+        return deq.getLast();
+    }
+    
+    public boolean isEmpty() {
+        return cnt == 0;
+    }
+    
+    public boolean isFull() {
+        return cnt == k;
+    }
+}
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque obj = new MyCircularDeque(k);
+ * boolean param_1 = obj.insertFront(value);
+ * boolean param_2 = obj.insertLast(value);
+ * boolean param_3 = obj.deleteFront();
+ * boolean param_4 = obj.deleteLast();
+ * int param_5 = obj.getFront();
+ * int param_6 = obj.getRear();
+ * boolean param_7 = obj.isEmpty();
+ * boolean param_8 = obj.isFull();
+ */
