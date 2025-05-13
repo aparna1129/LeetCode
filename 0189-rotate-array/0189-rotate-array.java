3015df51b1190ae36490fb74e931ce13f27b0bd9
aparna1129 <99273305+aparna1129@users.noreply.github.com>
2025-05-13 13:29:33 +0530

    Time: 1 ms (47.54%), Space: 57.1 MB (85.49%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,6 +1,16 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        k = k%nums.length;
+        if(k >= nums.length) k = k%nums.length;
+        // int[] temp = new int[k];
+        // for(int i = n-k; i < n; i++){
+        //     temp[i-(n-k)] = nums[i];
+        // }
+        // for(int i = k; i < n; i++){
+        //     nums[i] = nums[i-(k-i)];
+        // }
+        // for(int i = 0; i < k; i++){
+        //     nums[i] = temp[i];
+        // }
 
         reverse(nums, 0, nums.length - 1);
         reverse(nums, 0, k-1);
@@ -18,3 +28,4 @@ class Solution {
         }
     }
 }
+
