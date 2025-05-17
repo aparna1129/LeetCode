94e9271f5be580a1948f0dc0b8094b9fd452b08a
aparna1129 <99273305+aparna1129@users.noreply.github.com>
2025-05-17 13:45:08 +0530

    Time: 1 ms (99.93%), Space: 45.9 MB (77.55%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int singleNumber(int[] nums) {
+        int single = 0;
+        for(int i=0; i<nums.length; i++) {
+            single^=nums[i];
+        }
+        
+        return single;
+    }
+}
